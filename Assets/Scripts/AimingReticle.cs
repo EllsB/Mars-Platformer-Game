4e62b1ebf555a2cfe93c9f7ef7783888169349b8
ELISE AITCHISON <ellsaitchison@gmail.com>
2026-07-16 10:07:37 +0100

    Finished CRT filter

--- Assets/Scripts/AimingReticle.cs
+++ Assets/Scripts/AimingReticle.cs
@@ -4,14 +4,38 @@ using UnityEngine.InputSystem.XR;
 
 public class AimingReticle : MonoBehaviour
 {
+    [field: SerializeField] public bool locked { get; protected set; } = false;
     [field: SerializeField, Range(1f, 3f)] public float rangeMult { get; protected set; } = 1.5f;
     private Vector2 aimInput;
 
     public void onAim(InputAction.CallbackContext context)
     {
-        aimInput = context.ReadValue<Vector2>();
-        aimInput = aimInput * rangeMult;
-        Debug.Log($"Move Input: {aimInput}");
-        this.gameObject.transform.localPosition = aimInput;
+        if (!locked)
+        {
+            if (!this.gameObject.activeSelf)
+            {
+                this.gameObject.SetActive(true);
+            }
+            aimInput = context.ReadValue<Vector2>();
+            aimInput = aimInput * rangeMult;
+            Debug.Log($"Move Input: {aimInput}");
+            this.gameObject.transform.localPosition = aimInput;
+        }
+        if (aimInput.x == 0 && aimInput.y == 0)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    public void onLock(InputAction.CallbackContext context)
+    {
+        if (!locked && context.ReadValue<bool>())
+        {
+            locked = true;
+        }
+        else if (locked && context.ReadValue<bool>())
+        {
+            locked = false;
+        }
     }
 }
